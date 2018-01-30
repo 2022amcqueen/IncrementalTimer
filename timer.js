1bee4a61feb106ae82fbfd41a21d7c7c5aa2dc35
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2018-01-30 09:49:16 -0600

    Code update

--- timer.js
+++ timer.js
@@ -1,12 +1,12 @@
+//Variables
 var hr = 0
 var min = 0
 var sec = 0
-var ms = 0
 var element = document.getElementById("Time")
 var pause = false
 
+//Global Functions
 function timerState(){
-    
     if(pause == false){
         pause = true
     }else if(pause == true){
@@ -14,6 +14,16 @@ function timerState(){
     }
 }
 
+function reset(){
+    hr = 0
+    min = 0
+    sec = 0
+    
+    element.innerHTML = hr + " : " + min + " : " + sec
+}
+
+
+//Loop Function
 setInterval(function(){
     if(pause == false){
         if(min >= 59){
