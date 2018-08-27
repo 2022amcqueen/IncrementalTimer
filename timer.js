e6e52ffe044341ce1851c34f30e0b5f364465eb4
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2018-08-27 14:37:59 -0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -11,7 +11,6 @@ var localStorage = window.localStorage
 
 //Change Timer State 
 function timerState(){
-    
     if(pause == false){
         pause = true
         pauseElement.value = "Unpause"
@@ -19,19 +18,10 @@ function timerState(){
         pauseElement.value = "Pause"
         pause = false
     }
-    
-    /*
-    pause = !pause
-    if(pause){
-        pauseElement.value = "Unpause"
-    } else {
-        pauseElement.value = "Pause"
-    }
-    */
-    //pauseElement.value = (pause && "Unpause") || "Pause"
 }
 
 //Format Text (x to 0x)
+
 function format(str){
     if(str <= 9){
         return "0" + str
@@ -46,25 +36,21 @@ function reset(){
     min = 0
     sec = 0
     
-    element.innerHTML = ("00 : 00 : 00")//`${format(hr)} : ${format(min)} : ${format(sec)}`//(hr + " : " + min + " : " + sec)
+    element.innerHTML = (hr + " : " + min + " : " + sec)
 }
 
 //Add Time Function
 setInterval(function(){
-    if(!pause){
+    if(pause == false){
         
         min = ((min + (1/60)) % 60)
         sec = ((sec + 1) % 60)
         hr = ((hr + (1/600)) % 60)
         
         
-        var time = (format(Math.floor(hr)) + " : " + format(Math.floor(min)) + " : " + format(sec)))
+        var time = (format(Math.floor(hr)) + " : " + format(Math.floor(min)) + " : " + format(sec))
         
         element.innerHTML = time
         
     }
 }, 1000)
-
-
-
-
